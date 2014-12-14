e9617cac02050bc89b8c2de5f9ff7699e8333e7a
Sarat Tallamraju <saratt@Sarats-MacBook-Pro-2.local>
2014-12-13 18:03:31 -0800

    Initial Commit

---
+++ tipcalculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tipcalculator
+//
+//  Created by Sarat Tallamraju on 12/13/14.
+//  Copyright (c) 2014 sarattallamraju. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tipcalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tipcalculator
+//
+//  Created by Sarat Tallamraju on 12/13/14.
+//  Copyright (c) 2014 sarattallamraju. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
